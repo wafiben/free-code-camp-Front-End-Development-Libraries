6ba0f64a39253bf32eacb845c7e0c0a84b808f3c
wafiben <wafibenjeddou@gmail.com>
2024-11-27 19:49:17 +0100

    creaete project

---
+++ second-challenge/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Editor } from "./components/editor/editor";
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapped-bloc">
+        <Editor />
+      </div>
+    </div>
+  );
+}
+
+export default App;
